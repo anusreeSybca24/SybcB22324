543651bad768b646751609e8f958c9ad871f162b
AnuCasetools <anusree@rosarycollege.org>
2024-01-08 09:47:23 +0530

    first github commit

---
+++ sybcaB2Project/src/sybcaB2Project/clsGithub.java
@@ -0,0 +1,11 @@
+package sybcaB2Project;
+
+public class clsGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello world");
+	}
+
+}
