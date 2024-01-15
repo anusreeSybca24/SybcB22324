b364030480033c5d41977ae30f616e3f256d7610
AnuCasetools <anusree@rosarycollege.org>
2024-01-15 08:58:59 +0530

    commit branch

--- GithubProject/B3github/src/pkgB3Gihub/clSB3Demo.java
+++ GithubProject/B3github/src/pkgB3Gihub/clSB3Demo.java
@@ -4,9 +4,13 @@ public class clSB3Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world B3");
+		System.out.println("Hello world B2");
 
 	}
-
+    
+	 void display() 
+	 {
+		 System.out.println("branch B2");
+	 }
 	
 }
